c7eb52b50b6a7c814a341f7a745f88ebb4223e23
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-05-22 22:03:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	if (s2 === "") {
+        return 0;
+    }
+
+    // Convert both strings to lower case to perform a case-insensitive search
+    const lowerS1 = s1.toLowerCase();
+    const lowerS2 = s2.toLowerCase();
+
+    // Use indexOf to find the first occurrence of lowerS2 in lowerS1
+    return lowerS1.indexOf(lowerS2);
 }
 
 // Please do not change the code below
